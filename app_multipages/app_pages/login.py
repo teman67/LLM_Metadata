ad7d88de8cffd934b1bd58131fba148eed5f5a05
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-09-17 12:58:26 +0200

    update login page to get username and password from database

--- app_multipages/app_pages/login.py
+++ app_multipages/app_pages/login.py
@@ -1,6 +1,49 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
+import hashlib
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy import Column, Integer, String
+
+# Load environment variables
+load_dotenv()
+
+# Define the database URL
+DATABASE_URL = os.getenv('POSTGRESQL_Pass_URL')
+
+# Create a new SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a base class for the models
+Base = declarative_base()
+
+# Define the User model
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+
+# Create a new session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def check_credentials(username, password):
+    """Check if the provided username and password are correct."""
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    db = next(get_db())
+    user = db.query(User).filter(User.username == username, User.password == hashed_password).first()
+    return user is not None
 
 def login():
     # Initialize session state for login status if not already present
@@ -25,13 +68,6 @@ def login():
     title_placeholder = st.empty()
     title_placeholder.title("Login")
 
-    # Load environment variables from .env file
-    load_dotenv()
-
-    # Retrieve credentials from environment variables
-    USERNAME = os.getenv("APP_USERNAME")
-    PASSWORD = os.getenv("APP_PASSWORD")
-
     # Display login form
     with st.form(key='login_form'):
         username = st.text_input("Username")
@@ -39,7 +75,7 @@ def login():
         submit_button = st.form_submit_button("Login")
 
         if submit_button:
-            if username == USERNAME and password == PASSWORD:
+            if check_credentials(username, password):
                 st.session_state.logged_in = True
                 title_placeholder.empty()  # Clear the title on successful login
                 st.success("Login successful!")
