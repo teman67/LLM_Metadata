66db6d827091d6121f02f44dd7177f58bcc61ad6
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-09-29 19:12:37 +0200

    add docstring to multipage.py

--- app_multipages/app_pages/multipage.py
+++ app_multipages/app_pages/multipage.py
@@ -3,9 +3,21 @@ import streamlit as st
 
 # Class to generate multiple Streamlit pages using an object oriented approach
 class MultiPage:
-    
+    """
+    A class to generate multiple Streamlit pages using an object-oriented approach.
+
+    Attributes:
+        pages (list): A list to store the pages added to the app.
+        app_name (str): The name of the application.
+    """    
 
     def __init__(self, app_name) -> None:
+        """
+        Initializes the MultiPage class with the application name.
+
+        Args:
+            app_name (str): The name of the application.
+        """
         self.pages = []
         self.app_name = app_name
 
@@ -17,9 +29,22 @@ class MultiPage:
         # # https://twemoji.maxcdn.com/2/test/preview.html
 
     def add_page(self, title, func) -> None:
+        """
+        Adds a new page to the application.
+
+        Args:
+            title (str): The title of the page to be added.
+            func (callable): The function that renders the content of the page.
+        """
         self.pages.append({"title": title, "function": func})
 
     def run(self):
+        """
+        Runs the application, displaying the selected page based on user input.
+
+        The title of the application is displayed at the top, and a sidebar menu 
+        is created for page navigation.
+        """
         st.title(self.app_name)
         page = st.sidebar.radio('Menu', self.pages, format_func=lambda page: page['title'])
         page['function']()
