25772364219b13f54c191792aff0b30966a30eb3
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-10-14 15:30:04 +0200

    update the function name for LLM page

--- app_multipages/app.py
+++ app_multipages/app.py
@@ -10,15 +10,17 @@ from app_pages.page_register import registration_page
 from app_pages.page_LLM import *
 from app_pages.history import *
 from app_pages.page_json_viewer import *
+from app_pages.graph import graph_visualizer_page
 
 app = MultiPage(app_name="MetaData Retrieval")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Project Overview", page_summary_body)
 app.add_page("User Registration", registration_page)
-app.add_page("Explore Ollama Models", main)
+app.add_page("Explore Ollama Models", LLM_models)
 app.add_page("Chat History Overview", display_conversation_history)
 app.add_page("JSON File Viewer", json_viewer)
+app.add_page("Graph Visualizer", graph_visualizer_page)
 
 page_bg_img = '''
 <style>
